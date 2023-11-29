22e5c74772702095aa8be93193e7b8a9fc3a488c
Siddhant <siddhant.oswal@velsera.com>
2023-11-29 21:09:32 +0530

    commit 1

---
+++ src/reports/report.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
+}
