b262c2c998cede064edee75739fa7b125242020a
Siddhant <siddhant.oswal@velsera.com>
2023-12-01 20:32:17 +0530

    commit 4

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -3,7 +3,9 @@ import { Controller,
   Body, 
   UseGuards,
   Param,
-  Patch } from '@nestjs/common';
+  Patch,
+  Get, 
+  Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -13,11 +15,16 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
-import { Admin } from 'typeorm';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto){
+    console.log(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
